153baf44dbdf45e0c3fadbcdcf48e8174fc8456e
Heng Ma <maheng1991@hotmail.com>
2023-12-11 16:07:45 -0800

    added colmena run

---
+++ bvbrc_docking/parsl.py
@@ -0,0 +1,156 @@
+"""Utilities to build Parsl configurations."""
+from abc import ABC, abstractmethod
+from typing import Literal, Sequence, Tuple, Union
+
+from parsl.addresses import address_by_interface
+from parsl.config import Config
+from parsl.executors import HighThroughputExecutor
+from parsl.launchers import MpiExecLauncher
+from parsl.providers import LocalProvider, PBSProProvider
+
+from bvbrc_docking.utils import BaseModel, PathLike
+
+
+class BaseComputeSettings(BaseModel, ABC):
+    """Compute settings (HPC platform, number of GPUs, etc)."""
+
+    name: Literal[""] = ""
+    """Name of the platform to use."""
+
+    @abstractmethod
+    def get_config(self, run_dir: PathLike) -> Config:
+        """Create a new Parsl configuration.
+        Parameters
+        ----------
+        run_dir : PathLike
+            Path to store monitoring DB and parsl logs.
+        Returns
+        -------
+        Config
+            Parsl configuration.
+        """
+        ...
+
+
+class LocalSettings(BaseComputeSettings):
+    name: Literal["local"] = "local"  # type: ignore[assignment]
+    max_workers: int = 1
+    cores_per_worker: float = 0.0001
+    worker_port_range: Tuple[int, int] = (10000, 20000)
+    label: str = "htex"
+
+    def get_config(self, run_dir: PathLike) -> Config:
+        return Config(
+            run_dir=str(run_dir),
+            strategy=None,
+            executors=[
+                HighThroughputExecutor(
+                    address="127.0.0.1",
+                    label=self.label,
+                    cpu_affinity="block",
+                    max_workers=self.max_workers,
+                    cores_per_worker=self.cores_per_worker,
+                    worker_port_range=self.worker_port_range,
+                    provider=LocalProvider(init_blocks=1, max_blocks=1),  # type: ignore[no-untyped-call]
+                ),
+            ],
+        )
+
+
+class WorkstationSettings(BaseComputeSettings):
+    name: Literal["workstation"] = "workstation"  # type: ignore[assignment]
+    """Name of the platform."""
+    available_accelerators: Union[int, Sequence[str]] = 8
+    """Number of GPU accelerators to use."""
+    worker_port_range: Tuple[int, int] = (10000, 20000)
+    """Port range."""
+    retries: int = 1
+    label: str = "htex"
+
+    def get_config(self, run_dir: PathLike) -> Config:
+        return Config(
+            run_dir=str(run_dir),
+            retries=self.retries,
+            executors=[
+                HighThroughputExecutor(
+                    address="localhost",
+                    label=self.label,
+                    cpu_affinity="block",
+                    available_accelerators=self.available_accelerators,
+                    worker_port_range=self.worker_port_range,
+                    provider=LocalProvider(init_blocks=1, max_blocks=1),  # type: ignore[no-untyped-call]
+                ),
+            ],
+        )
+
+
+class PolarisSettings(BaseComputeSettings):
+    """Polaris settings. See here for details: https://docs.alcf.anl.gov/polaris/workflows/parsl/"""
+
+    name: Literal["polaris"] = "polaris"  # type: ignore[assignment]
+    label: str = "htex"
+
+    num_nodes: int = 1
+    """Number of nodes to request"""
+    worker_init: str = ""
+    """How to start a worker. Should load any modules and activate the conda env."""
+    scheduler_options: str = ""
+    """PBS directives, pass -J for array jobs"""
+    account: str
+    """The account to charge comptue to."""
+    queue: str
+    """Which queue to submit jobs to, will usually be prod."""
+    walltime: str
+    """Maximum job time."""
+    cpus_per_node: int = 64
+    """Up to 64 with multithreading."""
+    strategy: str = "simple"
+
+    def get_config(self, run_dir: PathLike) -> Config:
+        """Create a configuration suitable for running all tasks on single nodes of Polaris
+        We will launch 4 workers per node, each pinned to a different GPU.
+
+        Parameters
+        ----------
+            run_dir: PathLike
+                Directory in which to store Parsl run files.
+        """
+        return Config(
+            retries=1,  # Allows restarts if jobs are killed by the end of a job
+            executors=[
+                HighThroughputExecutor(
+                    label=self.label,
+                    heartbeat_period=15,
+                    heartbeat_threshold=120,
+                    worker_debug=True,
+                    max_workers=self.cpus_per_node,
+                    available_accelerators=4,  # Ensures one worker per accelerator
+                    address=address_by_interface("bond0"),
+                    cpu_affinity="block-reverse",
+                    prefetch_capacity=0,  # Increase if you have many more tasks than workers
+                    start_method="spawn",
+                    provider=PBSProProvider(  # type: ignore[no-untyped-call]
+                        launcher=MpiExecLauncher(  # Updates to the mpiexec command
+                            bind_cmd="--cpu-bind", overrides="--depth=64 --ppn 1"
+                        ),
+                        account=self.account,
+                        queue=self.queue,
+                        # PBS directives (header lines): for array jobs pass '-J' option
+                        scheduler_options=self.scheduler_options,
+                        worker_init=self.worker_init,
+                        nodes_per_block=self.num_nodes,
+                        init_blocks=1,
+                        min_blocks=0,
+                        max_blocks=1,  # Can increase more to have more parallel jobs
+                        cpus_per_node=self.cpus_per_node,
+                        walltime=self.walltime,
+                    ),
+                ),
+            ],
+            run_dir=str(run_dir),
+            strategy=self.strategy,
+            app_cache=True,
+        )
+
+
+ComputeSettingsTypes = Union[LocalSettings, WorkstationSettings, PolarisSettings]
---
+++ bvbrc_docking/run_colmena.py
@@ -0,0 +1,174 @@
+import logging
+import sys
+from argparse import ArgumentParser
+from functools import partial, update_wrapper
+from pathlib import Path
+from typing import List
+
+from colmena.models import Result
+from colmena.queue.python import PipeQueues
+from colmena.task_server import ParslTaskServer
+from colmena.thinker import BaseThinker, agent, result_processor
+from proxystore.store import register_store
+from proxystore.store.file import FileStore
+from pydantic import Field
+
+from bvbrc_docking.config import DockConfig
+from bvbrc_docking.parsl import ComputeSettingsTypes
+from bvbrc_docking.utils import BaseModel, mkdir_validator, path_validator
+
+
+def run_docking(pdb_file, output_dir, dock_type="fred", **kwargs):
+    if dock_type == "fred":
+        from bvbrc_docking.fred import fred_dock as docking
+    elif dock_type == "diffdock":
+        from bvbrc_docking.diffdock import diff_dock as docking
+    dock = docking(receptor_pdb=pdb_file, output_dir=output_dir, **kwargs)
+    dock.run()
+
+
+class ResultLogger:
+    def __init__(self, result_dir: Path) -> None:
+        result_dir.mkdir(exist_ok=True)
+        self.result_dir = result_dir
+
+    def log(self, result: Result, topic: str) -> None:
+        """Write a JSON result per line of the output file."""
+        with open(self.result_dir / f"{topic}.json", "a") as f:
+            print(result.json(exclude={"inputs", "value"}), file=f)
+
+
+class Thinker(BaseThinker):
+    def __init__(
+        self,
+        proteins: List[Path],
+        result_logger: ResultLogger,
+        n_workers: int = 1,
+        **kwargs,
+    ) -> None:
+        super().__init__(**kwargs)
+        self.proteins = proteins
+        self.n_workers = n_workers
+        self.task_idx = 0
+        self.result_logger = result_logger
+
+    def submit_docking_task(self):
+        if self.task_idx >= len(self.proteins):
+            self.done.set()
+            return
+
+        pdb_file = self.proteins[self.task_idx]
+
+        self.queues.send_inputs(
+            pdb_file,
+            method="run_docking",
+            topic="docking",
+            keep_inputs=False,
+        )
+        self.task_idx += 1
+
+    @agent(startup=True)
+    def start_tasks(self) -> None:
+        for i in range(self.n_workers):
+            self.submit_docking_task()
+
+    @result_processor(topic="docking")
+    def process_esm_result(self, result: Result):
+        self.result_logger.log(result, "docking")
+        if not result.success:
+            logging.warning(f"Bad inference result: {result.json()}")
+
+        # The old task is finished, start a new one
+        self.submit_docking_task()
+
+
+class WorkflowConfig(BaseModel):
+    input_dir: Path = Field(..., description="Path to protein pdbs")
+    output_dir: Path = Field(..., description="Path to the workflow output dir")
+    dock: DockConfig = Field(..., description="Docking config.")
+    compute_settings: ComputeSettingsTypes
+
+    # validators
+    _output_dir_mkdir = mkdir_validator("output_dir")
+    _protein_interactions_dir_exists = path_validator("input_dir")
+
+    def configure_logging(self) -> None:
+        """Set up logging."""
+        logging.basicConfig(
+            format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+            level=logging.INFO,
+            handlers=[
+                logging.FileHandler(self.output_dir / "runtime.log"),
+                logging.StreamHandler(sys.stdout),
+            ],
+        )
+
+
+if __name__ == "__main__":
+    parser = ArgumentParser()
+    parser.add_argument("-c", "--config", required=True)
+    args = parser.parse_args()
+    cfg = WorkflowConfig.from_yaml(args.config)
+    cfg.configure_logging()
+
+    # Make the proxy store
+    store = FileStore(name="file", store_dir=str(cfg.output_dir / "proxy-store"))
+    register_store(store)
+
+    # Make the queues
+    queues = PipeQueues(
+        serialization_method="pickle",
+        topics=["docking"],
+        proxystore_name="file",
+        proxystore_threshold=10000,
+    )
+
+    # Define the parsl configuration (this can be done using the get_config
+    # for common use cases or by defining your own configuration.)
+    parsl_config = cfg.compute_settings.get_config(f"{cfg.output_dir}/run-info")
+
+    dock_output = cfg.output_dir / "dock_result"
+    dock_output.mkdir()
+
+    my_run_docking = partial(
+        run_docking,
+        output_dir=dock_output,
+        **cfg.dock.dict(),
+    )
+    update_wrapper(my_run_docking, run_docking)
+
+    doer = ParslTaskServer([my_run_docking], queues, parsl_config)
+
+    # Create the result logger
+    result_logger = ResultLogger(cfg.output_dir / "result")
+
+    logging.info("Loading proteins")
+    proteins = sorted(list(cfg.input_dir.glob("*.pdb")))
+    thinker = Thinker(
+        queue=queues,
+        proteins=proteins,
+        n_workers=cfg.compute_settings.max_workers,
+        result_logger=result_logger,
+    )
+    logging.info("Created the task server and task generator")
+
+    try:
+        # Launch the servers
+        doer.start()
+        thinker.start()
+        logging.info("Launched the servers")
+
+        # Wait for the task generator to complete
+        thinker.join()
+        logging.info("Task generator has completed")
+    finally:
+        # Send the kill signal to the task server
+        queues.send_kill_signal()
+
+        # Wait for the task server to complete
+        doer.join()
+
+        # Clean up proxy store
+        store.close()
+
+    logging.info("Workflow has completed")
